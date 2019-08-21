3b6d6d9a35272ffbf41c6ede9718d88024ae5cf5
Pankaj Singh <pankajkumar.s@photoninfotech.net>
2019-08-21 16:58:10 +0530

    Initial commit

---
+++ complete-intro-to-react-v5/adopt-me/src/App.js
@@ -0,0 +1,34 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me !"),
+    [
+      React.createElement("h2", {}, "Adopt Me Again !!"),
+      React.createElement(Pet, {
+        name: "rusty",
+        animal: "dog",
+        breed: "havnese"
+      }),
+      React.createElement(Pet, {
+        name: "ketty",
+        animal: "cat",
+        breed: "mixed"
+      }),
+      React.createElement(Pet, {
+        name: "tile",
+        animal: "bird",
+        breed: "sparrow"
+      })
+    ]
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
